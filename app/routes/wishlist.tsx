84d9b2a72818f5e8a0c822b06a92c1c46d623621
Menard Maranan <menardmaranan.codes@gmail.com>
2024-07-06 22:37:46 +0800

    added delete route

--- app/routes/wishlist.tsx
+++ app/routes/wishlist.tsx
@@ -32,6 +32,29 @@ export async function action({ request, response }: LoaderFunctionArgs) {
                 return json({ error: 'Internal server error' }, { status: 500 });
             }
         }
+        case "DELETE": {
+            const errors = checkRequiredFormFields(formData, ["productId", "customerId", "shopDomain"]);
+            if (Object.keys(errors).length > 0) {
+                return json({ errors }, { status: 400 });
+            }
+
+            try {
+                await db.wishedProducts.delete({
+                    where: {
+                        productId_customerId_shopDomain:{
+                            productId: String(formData.get("productId")),
+                            customerId: String(formData.get("customerId")),
+                            shopDomain: String(formData.get("shopDomain")),
+                        }
+                    }
+                });
+                return json({ message: 'No content' }, { status: 204 });
+            } catch (error) {
+                // TODO: Logger
+                console.error(error);
+                return json({ error: 'Internal server error' }, { status: 500 });
+            }
+        }
         default: {
             return json({ error: 'Not implemented' }, { status: 501 });
         }
